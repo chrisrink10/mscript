a28c8d2c565f62e932991166ff279261d9b013af
Chris Rink <chrisrink10@gmail.com>
2016-05-19 12:07:29 -0400

    Implement command line arguments for the main mscript program driver

--- src/main.c
+++ src/main.c
@@ -20,16 +20,126 @@
 #include <readline/readline.h>
 #include "mscript.h"
 
-static int StartREPL(const char *prog) {
+typedef struct {
+    bool show_help;
+    bool show_version;
+    bool print_bytecode;
+    bool execute_string;
+    char *code;
+    bool execute_script;
+    char *script;
+    size_t nargs;
+    char **script_args;
+} CommandLineArgs;
+
+static void PrintHelp(const char *prog) {
+    printf("usage: %s -h -v -a -s [code] [script [args]]\n", prog);
+    puts("Options:");
+    puts("  -h        show this help text and exit");
+    puts("  -v        show the version and exit");
+    puts("  -a        print bytecode for all inputs");
+    puts("  -s [code] execute string `code`");
+}
+
+static void PrintVersion(void) {
+    puts("mscript v1.0");
+    puts("Copyright (c) 2016 Chris Rink");
+}
+
+static int ParseCommandLineOpts(CommandLineArgs *opts, int argc, char *argv[]) {
+    assert(opts);
+
+    for (int i = 1; i < argc; ) {
+        char *arg = argv[i];
+
+        if (arg[0] == '-') {
+            switch (arg[1]) {
+                case 'h':
+                    opts->show_help = true;
+                    i += 1;
+                    break;
+                case 'v':
+                    opts->show_version = true;
+                    i += 1;
+                    break;
+                case 'a':
+                    opts->print_bytecode = true;
+                    i += 1;
+                    break;
+                case 's':
+                    opts->execute_string = true;
+                    i += 1;
+                    if (i < argc) {
+                        opts->code = argv[i];
+                        i += 1;
+                    } else {
+                        printf("%s: expected argument `code`", argv[0]);
+                        PrintHelp(argv[0]);
+                        return EXIT_FAILURE;
+                    }
+                    break;
+                default:
+                    printf("%s: unrecognized option '-%c'\n", argv[0], arg[1]);
+                    PrintHelp(argv[0]);
+                    return EXIT_FAILURE;
+            }
+        } else {
+            opts->execute_script = true;
+            opts->script = argv[i];
+            i += 1;
+            opts->script_args = &argv[i];
+            opts->nargs = (size_t)(argc - i);
+            break;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
+
+static int ExecuteScript(const char *prog, CommandLineArgs *args) {
+    ms_StateOptions opts = {
+        .interactive_mode = false,
+        .print_bytecode = args->print_bytecode,
+    };
+    ms_State *ms = ms_StateNewOptions(&opts);
+    assert(ms);
+
+    const ms_Error *err;
+    if (ms_StateExecuteFile(ms, args->script, &err) == MS_RESULT_ERROR) {
+        printf("%s: \n%s\n", prog, err->msg);
+    }
+
+    ms_StateDestroy(ms);
+    return EXIT_SUCCESS;
+}
+
+static int ExecuteString(const char *prog, CommandLineArgs *args) {
+    ms_StateOptions opts = {
+        .interactive_mode = false,
+        .print_bytecode = args->print_bytecode,
+    };
+    ms_State *ms = ms_StateNewOptions(&opts);
+    assert(ms);
+
+    const ms_Error *err;
+    if (ms_StateExecuteString(ms, args->code, &err) == MS_RESULT_ERROR) {
+        printf("%s: \n%s\n", prog, err->msg);
+    }
+
+    ms_StateDestroy(ms);
+    return EXIT_SUCCESS;
+}
+
+static int StartREPL(const char *prog, CommandLineArgs *args) {
     ms_StateOptions opts = {
         .interactive_mode = true,
-        .print_bytecode = true,
+        .print_bytecode = args->print_bytecode,
     };
     ms_State *ms = ms_StateNewOptions(&opts);
     assert(ms);
 
     char *input;
-    printf("mscript v0.1\n");
+    puts("mscript v0.1");
     while ((input = readline("> ")) != NULL) {
         if (strlen(input) == 0) {
             free(input);
@@ -49,5 +159,28 @@ static int StartREPL(const char *prog) {
 }
 
 int main(int argc, char *argv[]) {
-    return StartREPL(argv[0]);
+    CommandLineArgs args = { 0 };
+    if (ParseCommandLineOpts(&args, argc, argv) == EXIT_FAILURE) {
+        return EXIT_FAILURE;
+    }
+
+    if (args.show_help) {
+        PrintHelp(argv[0]);
+        return EXIT_SUCCESS;
+    }
+
+    if (args.show_version) {
+        PrintVersion();
+        return EXIT_SUCCESS;
+    }
+
+    if (args.execute_string) {
+        return ExecuteString(argv[0], &args);
+    }
+
+    if (args.execute_script) {
+        return ExecuteScript(argv[0], &args);
+    }
+
+    return StartREPL(argv[0], &args);
 }
